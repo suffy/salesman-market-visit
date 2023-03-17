3b465c8525c774b2cd527e78a55fd7926a3bf343
ilhammsyah <ilhammsyah@gmail.com>
2023-03-17 14:32:05 +0700

    finish export denagan heading

---
+++ resources/views/beranda/briefing/exports/briefing.blade.php
@@ -0,0 +1,21 @@
+<table>
+    <thead>
+        <tr>
+            <th class="data-title">Tanggal</th>
+            <th class="data-title">Jenis</th>
+            <th class="data-title">Keterangan</th>
+            <th class="data-title">Created</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($briefing as $item)
+        <tr>
+            <td><span class="data-list">{{ $item->tgl_briefing }}</span></td>
+            <td><span class="data-list">{{ $item->jenis }}</span></td>
+            <td><span class="data-list">{!! $item->keterangan !!}</span></td>
+            <td><span class="data-list">{{ $item->created_by }}</span></td>
+        </tr>
+        @endforeach
+
+    </tbody>
+</table>
